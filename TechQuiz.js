f14723581fcc5f29025ab7857a0a513f779ce7c1
Excellence-daniel <43383067+Excellence-daniel@users.noreply.github.com>
2018-10-26 09:35:05 +0100

    Update TechQuiz.js

--- TechQuiz.js
+++ TechQuiz.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Countdown from 'react-countdown-now';
 
-import "./antdesign.css";
+import "./quiz.css";
 
 const Completionist = () => <span><center><b>No Time!</b></center></span>;
 
