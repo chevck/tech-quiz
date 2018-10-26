1d83c2160f843388f6fa5f4adcd4b4a59317c381
Excellence-daniel <43383067+Excellence-daniel@users.noreply.github.com>
2018-10-26 08:39:56 +0100

    Update TechQuiz.js
    Array of questions declared as Questions[]

--- TechQuiz.js
+++ TechQuiz.js
@@ -44,3 +44,46 @@ export class QuizQuestions extends React.Component {
       return <span style ={{fontSize: '18px'}}><b>{seconds}</b></span>;
     }
   };
+
+Questions = [
+    {
+      id: 0,
+      question: "What does CSS stand for?",
+      answers: [
+        "Computer Style Sheets",
+        "Creative Style Sheets",
+        "Cascading Style Sheets",
+        "Colorful Style Sheets"
+      ],
+      correct: "Cascading Style Sheets",
+      correctID: 2,
+      pointer: 0,
+      time: 0
+    },
+
+    {
+      id: 1,
+      question:
+        "Where in an HTML document is the correct place to refer to an external style sheet?",
+      answers: [
+        "In the <head> section",
+        "In the <body> section",
+        "At the end of the document",
+        "You can't refer to an external style sheet"
+      ],
+      correct: "In the <head> section",
+      correctID: 0,
+      pointer: 5,
+      time: 10
+    },
+
+    {
+      id: 2,
+      question: "Which HTML tag is used to define an internal style sheet?",
+      answers: ["<style>", "<script>", "<headStyle>", "<css>"],
+      correct: "<style>",
+      pointer: 4,
+      correctID: 0,
+      time: 20
+    }
+  ];
