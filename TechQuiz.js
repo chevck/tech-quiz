dc851719b15a91f33c510a06eb5f5f3baf940b8b
Excellence-daniel <43383067+Excellence-daniel@users.noreply.github.com>
2018-10-26 08:35:31 +0100

    Update TechQuiz.js
    The Completionist function declares the message given after time runs out

--- TechQuiz.js
+++ TechQuiz.js
@@ -3,3 +3,5 @@ import ReactDOM from "react-dom";
 import Countdown from 'react-countdown-now';
 
 import "./antdesign.css";
+
+const Completionist = () => <span><center><b>No Time!</b></center></span>;
