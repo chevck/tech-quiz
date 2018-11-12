96b661a86493c3b5bcc11ba1a89fbe689107e14a
Excellence-daniel <43383067+Excellence-daniel@users.noreply.github.com>
2018-11-12 18:28:40 +0100

    Add files via upload

---
+++ src/questions.js
@@ -0,0 +1,128 @@
+export const Questions = [
+    {
+      id: 0,
+      question: "What does CSS stand for?",
+      answers: [
+        "Computer Style Sheets",
+        "Creative Style Sheets",
+        "Cascading Style Sheets",
+        "Colorful Style Sheets"
+      ],
+      correct: "Cascading Style Sheets",
+      correctID: 2,
+      pointer: 0,
+      time: null
+    },
+
+    {
+      id: 1,
+      question:
+        "Where in an HTML document is the correct place to refer to an external style sheet?",
+      answers: [
+        "In the <head> section",
+        "In the <body> section",
+        "At the end of the document",
+        "You can't refer to an external style sheet"
+      ],
+      correct: "In the <head> section",
+      correctID: 0,
+      pointer: 5,
+      time: 5
+    },
+
+    {
+      id: 2,
+      question: "Which HTML tag is used to define an internal style sheet?",
+      answers: ["<style>", "<script>", "<headStyle>", "<css>"],
+      correct: "<style>",
+      pointer: 4,
+      correctID: 0,
+      time: 4
+    },
+
+    {
+      id: 3,
+      question: "Which company developed JavaScript?",
+      answers: ["Netscape", "Mozilla", "Andela", "Microsoft"],
+      correct: "Netscape",
+      pointer: 12,
+      correctID: 0,
+      time: 7
+    },
+
+    {
+      id: 4,
+      question: "The expression ('2' + 2) evaluates to: ",
+      answers: ["22", "4", "NaN", "undefined"],
+      correct: "22",
+      pointer: 10,
+      correctID: 0,
+      time: 15
+    },
+
+    {
+      id: 5,
+      question:
+        "Which loop construct loops at least once, before checking the loop condition?",
+      answers: ["for loop", "switch", "while loop", "do-while loop"],
+      correct: "do-while loop",
+      pointer: 29,
+      correctID: 3,
+      time: 10
+    },
+
+    {
+      id: 6,
+      question:
+        "Which of the following is correct in JavaScript",
+      answers: ['GETelementBYid', 'getElementById', 'getElementByID', 'GetElementById'],
+      correct: "getElementById",
+      pointer: 5,
+      correctID: 1,
+      time: 19
+    },
+
+    {
+      id: 7,
+      question:
+        "JavaScript runs on the ___________ side.",
+      answers: ['server', 'client', 'embeded', 'mobile'],
+      correct: "client",
+      pointer: 29,
+      correctID: 1,
+      time: 20
+    },
+
+    {
+      id: 8,
+      question:
+        "console.log(typeof [5, 'name', 87]) prints",
+      answers: ['number', 'mixed type', 'array', 'object'],
+      correct: "object",
+      pointer: 40,
+      correctID: 3,
+      time: 20
+    },
+
+    {
+      id: 9,
+      question:
+        "Choose the client-side JavaScript object:",
+      answers: ['Database', 'Cursor', 'Client', 'FileUpLoad'],
+      correct: "FileUpLoad",
+      pointer: 38,
+      correctID: 3,
+      time: 15
+    },
+
+    {
+      id: 10,
+      question:
+        "RAD stands for",
+      answers: ['Relative Application Development', 'Rapid Application Development', 'Rapid Application Document', 'Relative Application Document'],
+      correct: "Rapid Application Development",
+      pointer: 20,
+      correctID: 1,
+      time: 25
+  }
+  ];
